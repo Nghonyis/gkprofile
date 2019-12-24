4b8805a981ec4462c768e73f570c99c9bf157ec6
geekulchadev <tiyani@geekulcha.com>
2019-12-24 14:34:56 +0200

    something

--- port.js
+++ port.js
@@ -1,13 +1,3 @@
-/**
- * demo.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2018, Codrops
- * http://www.codrops.com
- */
 {
     // The Slide (Product) class.
     class Slide {
